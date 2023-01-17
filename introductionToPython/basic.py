f9004edeade0da07f0cd2b096a9377e4d4531a01
Abid Khan <abid.khan@kivihealth.com>
2023-01-17 09:52:30 +0530

    basic variable types

---
+++ introductionToPython/basic.py
@@ -0,0 +1,6 @@
+# --> Data types in python <--
+print(type("Hello")) # -string 
+print(type(1)) # Numbers - integers
+print(type(2.0)) # Numbers - floating point
+print(type(2+3j)) # NUmbers - complex
+print(type(True)) # bolean
