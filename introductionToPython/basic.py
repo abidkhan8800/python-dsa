0c84ca5a1e43cdb574f1c5e65d553f4b34894a5a
Abid Khan <abid.khan@kivihealth.com>
2023-01-17 10:00:33 +0530

    basic

--- introductionToPython/basic.py
+++ introductionToPython/basic.py
@@ -3,4 +3,4 @@ print(type("Hello")) # -string
 print(type(1)) # Numbers - integers
 print(type(2.0)) # Numbers - floating point
 print(type(2+3j)) # NUmbers - complex
-print(type(True)) # bolean
+print(type(True)) # boolean
